01346e3ebbbd81a76463611371cddbbad71a9450
hubert-levangong <hubertlvg@mac.com>
2019-10-29 16:24:17 -0700

    Prepping up for RSA key generation

--- bigint_test/src/main.rs
+++ bigint_test/src/main.rs
@@ -26,3 +26,25 @@ fn main() {
     println!("b: {}", b);
     println!("a*b: {}", a * b);
 }
+
+// nlen: intended length of the modulus n
+// exp: public verification exponent
+// seed: string of  2 * security_length (associated to nlen) bits from RBG
+fn prime_generator(nlen: usize, exp : u32, seed: Vec<u8>) -> BigUint {
+    let mut prime_candidate : BigUint = Zero::zero();
+
+    return prime_candidate;
+}
+
+
+fn is_prime(candidate : BigUint) -> bool {
+    return false;
+}
+
+
+// Returning twice the security strength bits of random data
+fn get_seed(nlen: usize) -> Vec<u8> {
+    let vect = Vec::with_capacity(2 * nlen);
+
+    return vect;
+}
