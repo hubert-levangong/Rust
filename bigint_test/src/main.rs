52ffffb2127cf3057d069a83c69a0e0b513e51ef
hubert-levangong <hubert@localhost.localdomain>
2019-10-29 15:05:24 -0700

    Using bigint and rand

--- bigint_test/src/main.rs
+++ bigint_test/src/main.rs
@@ -1,8 +1,10 @@
 extern crate num_bigint;
+extern crate rand;
 
-use num_bigint::BigUint;
+use num_bigint::{BigUint, ToBigInt, RandBigInt};
 use num_traits::{Zero, One};
 use std::mem::replace;
+
 
 fn main() {
     let n : usize = 5000;
@@ -14,4 +16,13 @@ fn main() {
         val0 = replace(&mut val1, val2);
     }
     println!(" val0: {}", val0);
+
+    let mut rng = rand::thread_rng();
+    let a = rng.gen_bigint(1000);
+    let low = 1.to_bigint().unwrap();
+    let high = 10000.to_bigint().unwrap();
+    let b = rng.gen_bigint_range(&low, &high);
+    println!("a: {}", a);
+    println!("b: {}", b);
+    println!("a*b: {}", a * b);
 }
