da6d9db1507c3bc646915d1bf3b6ca9aceea5ebf
hubert-levangong <hubertlvg@mac.com>
2019-11-15 17:23:39 -0800

    Challenge 6 - Added base 64 decode of input file, raw data is now in memory. Fixed the rest of code to use raw data. Stopped at blocks and transposed blocks generation implemented

--- CC/Challenge6/src/main.rs
+++ CC/Challenge6/src/main.rs
@@ -1,6 +1,7 @@
 use std::fs;
 use std::io::Read;
 use std::error::Error;
+use base64::{decode, decode_config};
 
 #[derive(Copy, Clone)]
 struct Distance {
@@ -8,30 +9,47 @@ struct Distance {
     dist: f32,
 }
 
+#[derive(Copy, Clone)]
+struct Freq {
+    code : u8,
+    occurences: i32,
+}
+
+// From steps 3 & 4
 // 3 smallest distances with corresponding key sizes
-// [3 : 2.3333333] <--- key size of 3 bytes
 // [2 : 2.5]
-// [7 : 2.5714285]
-const KEY_SIZE: usize = 3;
+// [3 : 2]
+// [5 : 1.2] <--- key size of 5 bytes
+
+const KEY_SIZE: usize = 5;
 
 fn main() {
-// Steps 1 & 2
+//    // Steps 1 & 2
 //    let in1 = Vec::from("this is a test");
 //    let in2 = Vec::from("wokka wokka!!!");
 //    let res = hamming_distance(&in1, &in2);
 //    println!("Hamming distance = {}", res);
 
-// Steps 3 & 4
+    // First we read the data from the file, remove EOL chars and Base64 decode it
+    let filename = String::from("/home/hubert/Documents/Github/Rust/CC/Challenge6/src/6.txt");
+    let mut fp = fs::File::open(filename).expect("Could not open the file");
+    let mut b64data = String::new();
+    let nbread = fp.read_to_string(&mut b64data).expect("Could not read the file");
+    let mut b64noEOLdata = b64data.replace("\n", "");
+    let rawdata = decode_config(&b64noEOLdata, base64::STANDARD).expect("Could not Base64 decode the data");
+    println!("Base64 decoded data is {} bytes long", rawdata.len());
+
+//    // Steps 3 & 4
 //    let mut dists= Vec::new();
-//    keysize_distances(2, 40, &mut dists);
+//    keysize_distances(&rawdata,2, 40, &mut dists);
 //    println!("result has {} distances", dists.len());
 //    for &elem in dists.iter() {
 //        println!("[{}:{}]", elem.keysz, elem.dist);
 //    }
 
-// Steps 5, 6 & 7
+    // Steps 5 & 6
     let mut blocks : Vec<Vec<u8>> = Vec::new();
-    fill_blocks(&mut blocks);
+    fill_blocks(&rawdata,&mut blocks);
     println!("Blocks: {:?}", blocks);
     let bl = blocks.len();
     let mut tblocks : Vec<Vec<u8>> = Vec::with_capacity(KEY_SIZE);
@@ -39,17 +57,19 @@ fn main() {
     transpose_blocks(&blocks, &mut tblocks);
     println!("Transposed Blocks: {:?}", tblocks);
 
+// Steps 7 & 8
+
 }
 
 
-fn keysize_distances(keysizemin : usize, keysizemax : usize, distances : &mut Vec<Distance>) {
+fn keysize_distances(rawdata : &Vec<u8>, keysizemin : usize, keysizemax : usize, distances : &mut Vec<Distance>) {
     println!("key sizes in ({}, {})", keysizemin, keysizemax);
     for keysize in keysizemin..keysizemax+1 {
         let mut in1 : Vec<u8> = Vec::with_capacity(keysize);
         in1.resize(keysize, 0);
         let mut in2 : Vec<u8> = Vec::with_capacity(keysize);
         in2.resize(keysize, 0);
-        fill_two_vectors(&mut in1, &mut in2).expect("Could not fill vectors with data.");
+        fill_two_vectors(rawdata, &mut in1, &mut in2);
         let val : f32 = hamming_distance(&in1, &in2) as f32 / (keysize as f32);
         let dst: Distance = Distance {keysz: keysize, dist: val};
         distances.push(dst);
@@ -84,33 +104,45 @@ fn count_bits(data : u8) -> u32 {
 }
 
 
-fn fill_two_vectors(vector1 : &mut Vec<u8>, vector2 : &mut Vec<u8>) -> Result<(), Box<dyn Error>> {
-    let filename = String::from("/home/hubert/Documents/Github/Rust/CC/Challenge6/src/6.txt");
-    let mut fp = fs::File::open(filename)?;
-
-    fp.read(vector1)?;
-    fp.read(vector2)?;
-
-    Ok(())
+fn fill_two_vectors(rawdata : &Vec<u8>, vector1 : &mut Vec<u8>, vector2 : &mut Vec<u8>) {
+    let nb = vector1.len();
+    for _i in 0..nb {
+        vector1[_i] = rawdata[_i];
+    }
+    for _i in 0..nb {
+        vector2[_i] = rawdata[nb+_i];
+    }
 }
 
 
-fn fill_blocks(liste : &mut Vec<Vec<u8>>) -> Result<(), Box<dyn Error>> {
-    let filename = String::from("/home/hubert/Documents/Github/Rust/CC/Challenge6/src/6.txt");
-    let mut fp = fs::File::open(filename)?;
-    let mut counter = 0;
-    let nbblocks = loop {
-        let mut vector: Vec<u8> = Vec::with_capacity(KEY_SIZE);
-        vector.resize(KEY_SIZE, 0);
-        let ret = fp.read(&mut vector)?;
-        if ret != KEY_SIZE {
-            break counter;
+fn fill_blocks(rawdata : &Vec<u8>, liste : &mut Vec<Vec<u8>>) {
+    let mut rawdataindex = 0;
+    let mut index = 0;
+    let mut vector : Vec<u8> = Vec::with_capacity(KEY_SIZE);
+    for _i in 0..rawdata.len() {
+        if index < KEY_SIZE { // keep filling current vector
+            vector.push(rawdata[_i]);
+            index += 1;
+        } else { // push the current vector and start loading a new one
+            liste.push(vector);
+            vector = Vec::with_capacity(KEY_SIZE);
+            vector.push(rawdata[_i]);
+            index = 1;
         }
-        counter += 1;
-        liste.push(vector);
-    };
-    println!("Counted {} blocks", nbblocks);
-    Ok(())
+    }
+
+//    let nbblocks = loop {
+//        let mut vector: Vec<u8> = Vec::with_capacity(KEY_SIZE);
+//        vector.resize(KEY_SIZE, 0);
+//        let ret = fp.read(&mut vector)?;
+//        if ret != KEY_SIZE {
+//            break counter;
+//        }
+//        counter += 1;
+//        liste.push(vector);
+//    };
+//    println!("Counted {} blocks", nbblocks);
+//    Ok(())
 }
 
 
@@ -123,4 +155,49 @@ fn transpose_blocks(bin : &Vec<Vec<u8>>, bout : &mut Vec<Vec<u8>>) {
             bout[_i].push(bin[_j][_i]);
         }
     }
+}
+
+
+fn get_most_frequent_code(data : &Vec<u8>) -> u8 {
+    let mut liste : Vec<Freq> = Vec::new();
+
+    // Calculate frequencies
+    for &item in data.iter() {
+        let index = in_liste(item, &liste);
+        if index != -1 {
+            liste[index as usize].occurences += 1;
+        } else {
+            let elem : Freq = Freq {code: item, occurences: 1};
+            liste.push(elem);
+        }
+    }
+
+    // going through list of occurences to find the top one
+    if liste.len() == 0 { return 0};
+    let mut top = liste[0];
+    for &item in liste.iter() {
+        if item.occurences > top.occurences {
+            top = item;
+        }
+    }
+    return top.code;
+}
+
+
+fn in_liste(code : u8, liste : &Vec<Freq>) -> i32 {
+    for (i, elem) in liste.iter().enumerate() {
+        if code == elem.code { return i as i32}
+    }
+    return -1;
+}
+
+fn fixed_xor(buf1 : Vec<u8>, buf2 : Vec<u8>) -> Vec<u8> {
+    let mut res = Vec::new();
+    if buf1.len() != buf2.len() {
+        panic!("Lenghts differ.")
+    }
+    for (i, &_item) in buf1.iter().enumerate() {
+        res.push(buf1[i] ^ buf2[i]);
+    }
+    return res;
 }
